5461230ff4cc50646a640be19703ee2c16a53092
Jerry Davidson <willdjerry@gmail.com>
2020-05-03 01:52:44 -0400

    API calls work for get and post requests to the local server and weather API. The site is mobile responsive for all devices

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {
+    posts:[]
+};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const listening = () => { console.log(`Using localhost: ${port}`) };
+const server = app.listen(port, listening);
+
+
+app.get('/posts',(req, res) => {
+    res.send(projectData.posts)
+})
+
+app.post('/entry',(req,res) => {
+    projectData.posts.push(req.body)
+    res.send({"status":"posted",projectData})
+})
+
+
+
--- website/app.js
+++ website/app.js
@@ -1,5 +1,124 @@
 /* Global Variables */
+const apiKey = '';
+
+
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+
+const toggleShow = () => {
+    // hide journal entry input
+    let closeButton = document.querySelector('.close-button');
+    let journalInput = document.querySelector('.journal-input-container');
+    closeButton.addEventListener('click', function (e) {
+        e.preventDefault()
+        journalInput.classList.add('hidden');
+        addNewButton.classList.remove('hidden');
+    });
+
+    // show journal entry input
+    let addNewButton = document.querySelector('.add-entry-button')
+    addNewButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        addNewButton.classList.add('hidden');
+        journalInput.classList.remove('hidden');
+    });
+
+}
+setTimeout(toggleShow, 0);
+
+const getTemp = async (key, zip) => {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${key}`)
+        .then(res => { return res.json() })
+    return response.main.temp;
+}
+const postEntry = async (header, date, temp, zip, content) => {
+    const addPost = await fetch('http://localhost:3000/entry', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "header": header,
+            "date": date,
+            "temp": temp,
+            "zip": zip,
+            "content": content
+        })
+    }).then(res => {return res.json()});
+    console.log(addPost)
+}
+const renderPosts = (header,date,temp,zip,content) => {
+    const fragment = document.createDocumentFragment();
+    let journalEntries = document.querySelector('.journal-entries');
+
+    const headerElement = document.createElement('h1');
+    const paragraphElement = document.createElement('p');
+    // const zipElement = document.createElement('span');
+    const tempElement = document.createElement('span');
+    const dateElement = document.createElement('span');
+    const container = document.createElement('div');
+    const subContainer = document.createElement('div');
+
+
+    // add classes
+    subContainer.classList.add('sub-container');
+    // zipElement.classList.add('zip');
+    tempElement.classList.add('temp');
+    dateElement.classList.add('date');
+    
+    //set inner text for elements
+    headerElement.innerText = header;
+    container.appendChild(headerElement);
+
+    // zipElement.innerText = zip;
+    // subContainer.appendChild(zipElement)
+
+    dateElement.innerText = date
+    subContainer.appendChild(dateElement)
+
+    tempElement.innerText = temp
+    subContainer.appendChild(tempElement)
+
+    container.appendChild(subContainer)
+
+    paragraphElement.innerText = content;
+    container.appendChild(paragraphElement);
+    // add container to fragment
+    fragment.appendChild(container)
+    // add fragment to parent
+    journalEntries.prepend(fragment);
+}
+
+const getPosts = async () => {
+    const posts = await fetch('http://localhost:3000/posts').then(res => {return res.json()});
+    console.log(posts)
+    for(post of posts){
+        renderPosts(post.header,
+            post.date,
+            post.temp,
+            post.zip,
+            post.content)
+    }
+}
+setTimeout(getPosts,0);
+
+addPost = () => {
+    const submit = document.querySelector('.submit-button');
+    submit.addEventListener('click', async function () {
+        let title = document.querySelector('.journal-title-input').value;
+        let zipCode = document.querySelector('.journal-zip-input').value;
+        let content = document.querySelector('.journal-content-input').value;
+        let temp = await getTemp(apiKey, zipCode);
+
+        // adds post to backend
+        postEntry(title, newDate, temp, zipCode, content);
+        // adds post to UI
+        renderPosts(title,newDate,temp,zipCode,content)
+    })
+}
+setTimeout(addPost, 0);
